8ff552cc3dc11d02ba2ee78a307a1078617b88be
Rin <62451491+FullowID@users.noreply.github.com>
2022-05-28 23:20:23 +0800

    Upload modul 6: Session

---
+++ Modul6/ErrorPage.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Perpustakaan</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar navbar-inverse bg-secondary bg-opacity-25">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Perpustakaan</a>
+    </div>
+  </nav>
+  <div class="position-absolute top-50 start-50 translate-middle text-center">
+    <h2 class="">Guest user tidak memiliki akses untuk melihat data</h2>
+    <a href='FormLogin.php'><button class="btn btn-primary text-white">Form Login</button></a>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
